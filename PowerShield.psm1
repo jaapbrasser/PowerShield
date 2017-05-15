02782cd4886fe6f924967d186bad2c248a655c4b
Jaap Brasser <noreply@jaapbrasser.com>
2017-05-16 00:52:52 +0200

    Cleaned up comments

--- PowerShield.psm1
+++ PowerShield.psm1
@@ -1,10 +1,10 @@
-# All internal functions privately available within the toolset - 221ms
+# All internal functions
 foreach ($function in (Get-ChildItem "$PSScriptRoot\internal\*.ps1"))
 {
 	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
 }
 
-# All exported functions - 600ms
+# All exported functions
 foreach ($function in (Get-ChildItem "$PSScriptRoot\functions\*.ps1"))
 {
 	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
