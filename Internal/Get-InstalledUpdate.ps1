54e44bdfef2deda78f94146bd93dd9a274000282
Jaap Brasser <noreply@jaapbrasser.com>
2017-05-19 13:20:02 +0200

    Updated code

--- Internal/Get-InstalledUpdate.ps1
+++ Internal/Get-InstalledUpdate.ps1
@@ -1,3 +1,9 @@
 function Get-UpdateInstalled {
-
+    if (Get-Command Get-HotFix -ErrorAction SilentlyContinue) {
+        Get-HotFix |
+        Select-Object -ExpandProperty
+    } else {
+        Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Updates -Recurse |
+        Select-Object -ExpandProperty Name
+    }
 }
